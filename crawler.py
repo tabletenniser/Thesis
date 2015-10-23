6a30059fcc3a5654f16204d50fc90255b321281d
tabletenniser <tabletenniser@gmail.com>
2015-10-22 23:47:12 -0400

    Initial Commit

---
+++ crawler.py
@@ -0,0 +1,56 @@
+from pytube import YouTube
+import os
+import time
+import argparse
+from pytube.utils import print_status
+from pprint import pprint
+
+# [<Video: MPEG-4 Visual (.3gp) - 144p>,
+#  <Video: MPEG-4 Visual (.3gp) - 240p>,
+#  <Video: Sorenson H.263 (.flv) - 240p>,
+#  <Video: H.264 (.flv) - 360p>,
+#  <Video: H.264 (.flv) - 480p>,
+#  <Video: H.264 (.mp4) - 360p>,
+#  <Video: H.264 (.mp4) - 720p>,
+#  <Video: VP8 (.webm) - 360p>,
+#  <Video: VP8 (.webm) - 480p>]
+
+def download_video(yt, path, file_name):
+    yt.set_filename(file_name)
+
+    # print(yt.filter('mp4')[-1])
+    # pprint(yt.filter(resolution='480p'))
+    # video = yt.get('mp4', '720p')
+    # video.download()
+    # yt.videos[0].download()
+    try:
+        # yt.videos[0].download(path=os.getcwd(), on_progress=print_status)
+        yt.get('mp4', '360p').download(path=path, on_progress=print_status)
+    except KeyboardInterrupt:
+        print("Download interrupted.")
+    # Note: If you wanted to choose the output directory, simply pass it as an
+    # argument to the download method.
+
+if __name__ == '__main__':
+    start_time = time.time()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("url_file", type=str, help='Path to the url.txt file')
+    parser.add_argument("output_dir", type=str, help='Output directory where the downloaded .mp4 files go')
+    args = parser.parse_args()
+
+    file_name = args.url_file
+    print "file_name is: ", file_name
+    with open(file_name) as f:
+        for line in f:
+            if line[0] == "#":
+                prev_line = line
+                continue
+            print "========= DOWNLOAD YOUTUBE VIDEO ========="
+            print "Link: "+line[:-1]
+            print "Name: "+prev_line[2:-1]
+            yt = YouTube(line)
+            # download_video(yt, os.getcwd(), prev_line[2:])
+            download_video(yt, args.output_dir, prev_line[2:-1])
+            prev_line = line
+    print "TOTAL EXECUTION TIME:"+str(time.time() - start_time)+" seconds"
+
---
+++ decompose_to_frames.py
@@ -0,0 +1,53 @@
+import cv2
+import argparse
+import os
+import time
+
+FRAME_GAP = 1
+START_FRAME = 100
+
+# video_path is a .mp4 file; output_dir is the directory containing all the output frames.
+def extract_frames(video_path, output_dir):
+    print "Writing video: "+str(video_path)+" to "+str(output_dir)
+    vidcap = cv2.VideoCapture(video_path)
+    # image is an array of array of [R,G,B] values]
+    success,image = vidcap.read()
+    count = START_FRAME;
+    while success:
+        success,image = vidcap.read()
+        # print "count is"+str(count)
+        if count % FRAME_GAP ==0:
+            file_path = os.path.join(output_dir, "frame%d.jpg" % count)
+            print "Writing to file"+str(file_path)
+            cv2.imwrite(file_path, image)     # save frame as JPEG file
+        if cv2.waitKey(10) == 27:             # exit if Escape is hit
+            break
+        count += 1
+
+
+if __name__ == '__main__':
+    start_time = time.time()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_dir", type=str, help='Path to the directory where .mp4 files locate')
+    parser.add_argument("output_dir", type=str, help='Path to where the .jpg frames will be output to')
+    args = parser.parse_args()
+    args.input_dir = os.path.abspath(args.input_dir)
+    args.output_dir = os.path.abspath(args.output_dir)
+    print "Input directory is ", args.input_dir
+    print "Output directory is ", args.output_dir
+
+    for fn in os.listdir(args.input_dir):
+        video_file_path = os.path.join(args.input_dir, fn)
+        video_file_name, video_file_extension = os.path.splitext(video_file_path)
+        if not video_file_extension==".mp4":
+            print "Skip non .mp4 file", video_file_path
+            continue
+        print "Input video path is: ", video_file_path
+        output_path = os.path.join(args.output_dir, os.path.basename(video_file_name))
+        print "Output path is: ", output_path
+        if os.path.isdir(output_path):
+            print "ERROR: OUTPUT PATH ALREADY EXISTS!!! Program exist"
+            exit()
+        os.makedirs(output_path)
+        extract_frames(video_file_path, output_path)
+    print "TOTAL EXECUTION TIME:"+str(time.time()-start_time)+" seconds"
