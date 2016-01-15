6860fc0efba1d3cc498398823fa061e254349a02
tabletenniser <zexuan.wang@mail.utoronto.ca>
2016-01-15 17:45:49 -0500

    Add new training data for score recognition, 18*18 images with shift and different sizes

--- training_set_creation_for_score_recog.py
+++ training_set_creation_for_score_recog.py
@@ -2,16 +2,15 @@ try:
     import Image
 except ImportError:
     from PIL import Image
-from PIL import ImageEnhance
 import time
 import argparse
 import os
+import numpy as np
 
-
-WIDTH = 20
-HEIGHT = 20
+WIDTH = 18
+HEIGHT = 18
 DELTA_WIDTH = 4
-DELTA_HEIGHT = 4
+DELTA_HEIGHT = 2
 
 def create_folder_if_not_exist(path):
     if not os.path.isdir(path):
@@ -36,12 +35,30 @@ def main(input_dir, output_dir):
             # if not pic.endswith('png'):
             #     continue
             im = Image.open(os.path.join(input_folder_path, pic))
+            print 'Processing image: ', pic
             counter = 0
-            for x in xrange(DELTA_WIDTH):
-                for y in xrange(DELTA_HEIGHT):
-                    im2 = im.crop((x, y, x+WIDTH, y+HEIGHT)).convert('1')
-                    im2.save(os.path.join(output_folder_path, "%d_%d_%02d.png" % (int(folder), im_counter, counter)))
-                    counter+=1
+            im_width, im_height = im.size
+            for size in xrange(18, 25):
+                for x in xrange(-DELTA_WIDTH, DELTA_WIDTH+1):
+                    x_left = (im_width-size)/2+x
+                    if x_left < 0 or x_left+size > im_width:
+                        continue
+                    for y in xrange(-DELTA_HEIGHT, DELTA_HEIGHT+1):
+                        y_top = (im_height-size)/2+y
+                        if y_top < 0 or y_top+size > im_height:
+                            continue
+                        im2 = im.crop((x_left, y_top, x_left+size, y_top+size)).convert('L')
+                        print 'Create image of square: ', (x_left, y_top, x_left+size, y_top+size)
+                        # bw_threshold = 0.3
+                        # im_max=np.amax(np.asarray(im2))
+                        # im_min=np.amin(np.asarray(im2))
+                        # im2 = im2.point(lambda x: 0 if x<((im_max-im_min)*bw_threshold+im_min) else 255, '1')
+                        im2 = im2.resize((WIDTH, HEIGHT), Image.ANTIALIAS)
+                        im2_array = np.asarray(im2)
+                        im2_array_mean = im2_array.mean()
+                        im2 = im2.point(lambda x: 0 if x < im2_array_mean else 255, '1')
+                        im2.save(os.path.join(output_folder_path, "%d_%d_%02d.png" % (int(folder), im_counter, counter)))
+                        counter+=1
             im_counter += 1
 
 if __name__=='__main__':
