0d5f0bb598d5d995e71eafab4d6648693842ad9e
tabletenniser <zexuan.wang@mail.utoronto.ca>
2016-02-07 01:05:35 -0500

    add failed detection of end of a point using audio info

--- training_set_preparation_postprocessing.py
+++ training_set_preparation_postprocessing.py
@@ -7,8 +7,11 @@ import argparse
 import os
 import numpy as np
 import shutil
+from scipy.io.wavfile import read,write
+from numpy.fft import rfft, irfft
+from scipy.signal import blackmanharris, fftconvolve
 
-SAMPLE_RATE = 10
+SAMPLE_RATE = 20
 
 def create_folder_if_not_exist(path):
     if not os.path.isdir(path):
@@ -71,22 +74,49 @@ def freq_from_fft(sig, fs):
     f = rfft(windowed)
 
     # Find the peak and interpolate to get a more accurate peak
-    i = argmax(abs(f)) # Just use this for less-accurate, naive version
-    true_i = parabolic(log(abs(f)), i)[0]
+    i = np.argmax(abs(f)) # Just use this for less-accurate, naive version
+    # true_i = parabolic(log(abs(f)), i)[0]
 
     # Convert to equivalent frequency
-    return fs * true_i / len(windowed)
+    return fs * i / len(windowed)
 
-def plotFrequency(y,Fs):
+def freq_over_time(y,Fs):
     resultFreq = []
     x_val = []
     start_num = 0
     while start_num+2*Fs < len(y):
         resultFreq.append(freq_from_fft(y[:,1][start_num: start_num+2*Fs], Fs))
         x_val.append(start_num)
-        start_num += Fs/16
-    print resultFreq
-###### CODE BELOW SELECTS THE LAST 10 FRAMES OF EACH POINT AND PUT INTO THE *_selected FOLDER #####
+        start_num += Fs/SAMPLE_RATE
+    return resultFreq
+
+###### CODE BELOW SELECTS LAST SAMPLE_RATE FRAMES (last second) OF EACH POINT AND PUT INTO THE *_selected FOLDER #####
+def select_images_last_10(output_image_dir):
+    assert(os.path.isdir(output_image_dir))
+    folders = [f for f in sorted(os.listdir(output_image_dir)) if os.path.isdir(os.path.join(output_image_dir, f))]
+    # folders should contain bottom_player_winning and top_player_winning.
+
+    for folder in folders:
+        output_selected_image_path = os.path.join(output_image_dir, folder+'_selected')
+        create_folder_if_not_exist(output_selected_image_path)
+        output_image_path = os.path.join(output_image_dir, folder)
+        assert(os.path.isdir(output_image_path))
+
+        image_folders = [f for f in sorted(os.listdir(output_image_path)) if os.path.isdir(os.path.join(output_image_path, f))]
+        for img_folder in image_folders:
+            full_output_image_path = os.path.join(output_image_path, img_folder)
+
+            files_to_copy = sorted(os.listdir(full_output_image_path))
+            if len(files_to_copy) < 10:
+                continue
+            for f in files_to_copy[-10:]:
+                src = os.path.join(full_output_image_path, f)
+                dest = os.path.join(output_selected_image_path, img_folder+'_'+f)
+                print "DEBUG: copy ", src, " to ", dest
+                shutil.copy(src, dest)
+
+
+###### CODE BELOW SELECTS 10 FRAMES OF EACH POINT AND PUT INTO THE *_selected FOLDER #####
 def select_images(output_image_dir, output_audio_dir):
     assert(os.path.isdir(output_image_dir))
     assert(os.path.isdir(output_audio_dir))
@@ -96,16 +126,37 @@ def select_images(output_image_dir, output_audio_dir):
     for folder in folders:
         output_selected_image_path = os.path.join(output_image_dir, folder+'_selected')
         create_folder_if_not_exist(output_selected_image_path)
+        output_image_path = os.path.join(output_image_dir, folder)
+        output_audio_path = os.path.join(output_audio_dir, folder)
+        assert(os.path.isdir(output_image_path))
+        assert(os.path.isdir(output_audio_path))
+
         image_folders = [f for f in sorted(os.listdir(output_image_path)) if os.path.isdir(os.path.join(output_image_path, f))]
         for img_folder in image_folders:
             full_output_image_path = os.path.join(output_image_path, img_folder)
+            full_output_audio_file = os.path.join(output_audio_path, img_folder+'.wav')
+            rate, data = read(full_output_audio_file)
+            freq_array = freq_over_time(data, rate)
+            print 'audio file: ', full_output_audio_file
+            print 'freq array: ', freq_array
+            # Iterate through all frequencies and find the first time where frequency goes under 250Hz from the back
+            index = len(freq_array)-1
+            while True:
+                if index == 0:
+                    index = len(freq_array)-1
+                    break
+                if freq_array[index] < 250 and freq_array[index-1] < 250 and freq_array[index-2] < 250:
+                    break
+                index -= 1
+
             files_to_copy = sorted(os.listdir(full_output_image_path))
-            for f in files_to_copy[-10:]:
+            print 'files_to_copy.size(): ', len(files_to_copy), 'freq_array.size(): ', len(freq_array)
+            # prop_index = len(freq_array)-1-index
+            for f in files_to_copy[index+11:index+21]:  # +21 because frequency is taken over 2 seconds
                 src = os.path.join(full_output_image_path, f)
                 dest = os.path.join(output_selected_image_path, img_folder+'_'+f)
                 print "DEBUG: copy ", src, " to ", dest
                 shutil.copy(src, dest)
-
 
 if __name__=='__main__':
     start_time = time.time()
@@ -120,6 +171,7 @@ if __name__=='__main__':
     args.image_dir = os.path.abspath(args.image_dir)
     args.video_dir = os.path.abspath(args.video_dir)
     args.audio_dir = os.path.abspath(args.audio_dir)
-    crop_size_for_video_and_images(args.input_dir, args.image_dir, args.video_dir, args.audio_dir)
+    # crop_size_for_video_and_images(args.input_dir, args.image_dir, args.video_dir, args.audio_dir)
     # select_images(args.image_dir, args.audio_dir)
+    select_images_last_10(args.image_dir)
     print "TOTAL EXECUTION TIME:"+str(time.time()-start_time)+" seconds"
