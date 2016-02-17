56908d4e085bdd42c45f3fb8176075b83397711f
tabletenniser <zexuan.wang@mail.utoronto.ca>
2016-02-17 01:18:25 -0500

    update training label and new postprocessing script

--- move_training_set.py
+++ move_training_set.py
@@ -3,8 +3,42 @@ import argparse
 import os
 import shutil
 
-TOP_WINNING_DIRECTORY='top_player_winning'
-BOTTOM_WINNING_DIRECTORY='bottom_player_winning'
+CLASSES = ['c1_top_player_forehand_serve',
+'c2_top_player_backhand_serve',
+'c3_bottom_player_forehand_serve',
+'c4_bottom_player_backend_serve',
+'c5_top_player_forehand_loop',
+'c6_top_player_backhand_loop',
+'c7_bottom_player_forehand_loop',
+'c8_bottom_player_backend_loop',
+'c9_top_player_forehand_block',
+'c10_top_player_backhand_block',
+'c11_bottom_player_forehand_block',
+'c12_bottom_player_backend_block',
+'c13_top_player_forehand_flip',
+'c14_top_player_backhand_flip',
+'c15_bottom_player_forehand_flip',
+'c16_bottom_player_backend_flip',
+'c17_top_player_forehand_chop',
+'c18_top_player_backhand_chop',
+'c19_bottom_player_forehand_chop',
+'c20_bottom_player_backend_chop',
+'c21_top_player_forehand_under_net',
+'c22_top_player_forehand_hit_out',
+'c23_top_player_forehand_miss_hit',
+'c24_top_player_backend_under_net',
+'c25_top_player_backend_hit_out',
+'c26_top_player_backend_miss_hit',
+'c27_bottom_player_forehand_under_net',
+'c28_bottom_player_forehand_hit_out',
+'c29_bottom_player_forehand_miss_hit',
+'c30_bottom_player_backend_under_net',
+'c31_bottom_player_backend_hit_out',
+'c32_bottom_player_backend_miss_hit',
+'c33_bottom_player_backend_lob',
+'c34_top_player_forehand_smash',
+'top_player_winning',
+'bottom_player_winning']
 
 def create_dir_if_not_exist(dir_path):
     if not os.path.isdir(dir_path):
@@ -16,11 +50,12 @@ def copy_frames_to_destination(src_dir, destination_dir, start_index):
     frame_dirs = [os.path.join(src_dir, d) for d in sorted(os.listdir(src_dir)) if os.path.isdir(os.path.join(src_dir, d))]
     for frame_dir in frame_dirs:
         basename = os.path.basename(os.path.normpath(frame_dir))
-        mp4_file_src = os.path.join(src_dir, basename+'.mp4')
-        if os.path.exists(mp4_file_src):
-            mp4_file_dest = os.path.join(destination_dir, 'point_%05d.mp4'%index)
-            shutil.copy(mp4_file_src, mp4_file_dest)
-            print 'Copy mp4 file ', mp4_file_src, ' to destionation', mp4_file_dest
+        # DO NOT COPY .mp4 VIDEOS FOR NOW
+        # mp4_file_src = os.path.join(src_dir, basename+'.mp4')
+        # if os.path.exists(mp4_file_src):
+        #     mp4_file_dest = os.path.join(destination_dir, 'point_%05d.mp4'%index)
+        #     shutil.copy(mp4_file_src, mp4_file_dest)
+        #     print 'Copy mp4 file ', mp4_file_src, ' to destionation', mp4_file_dest
         dest_dir = os.path.join(destination_dir, 'point_%05d'%index)
         if os.path.exists(dest_dir):
             shutil.rmtree(dest_dir)
@@ -34,24 +69,22 @@ def main(input_dir, output_dir):
     print 'move_training_set.py called with:\n', input_dir, '\n', output_dir
     start_time = time.time()
     # Copy points frames and .mp4 over to the final directory
-    top_winning_destination_dir = os.path.join(output_dir, TOP_WINNING_DIRECTORY)
-    bottom_winning_destination_dir = os.path.join(output_dir, BOTTOM_WINNING_DIRECTORY)
-    create_dir_if_not_exist(top_winning_destination_dir)
-    create_dir_if_not_exist(bottom_winning_destination_dir)
-    # print 'top_winning_destination_dir: ', top_winning_destination_dir
-    # print 'bottom_winning_destination_dir: ', bottom_winning_destination_dir
+    destination_folders = []
+    for cls in CLASSES:
+        destination_folders.append(os.path.join(output_dir, cls))
+        create_dir_if_not_exist(os.path.join(output_dir, cls))
 
     video_dirs = [os.path.join(input_dir, f) for f in sorted(os.listdir(input_dir)) if os.path.isdir(os.path.join(input_dir, f))]
     # print video_dirs
+    indices = [1 for _ in xrange(len(CLASSES))]
     pt_index_top = 1
     pt_index_bottom = 1
     for video_dir in video_dirs:
-        top_winning_source_dir = os.path.join(video_dir, TOP_WINNING_DIRECTORY)
-        bottom_winning_source_dir = os.path.join(video_dir, BOTTOM_WINNING_DIRECTORY)
-        # print 'top_winning_source_dir: ', top_winning_source_dir
-        # print 'bottom_winning_source_dir: ', bottom_winning_source_dir
-        pt_index_top = copy_frames_to_destination(top_winning_source_dir, top_winning_destination_dir, pt_index_top)
-        pt_index_bottom = copy_frames_to_destination(bottom_winning_source_dir, bottom_winning_destination_dir, pt_index_bottom)
+        # for cls in CLASSES:
+        for i in xrange(len(CLASSES)):
+            source_dir = os.path.join(video_dir, CLASSES[i])
+            if os.path.exists(source_dir):
+                indices[i] = copy_frames_to_destination(source_dir, destination_folders[i], indices[i])
 
     print "MOVE_TRAINING_SET.PY TAKES:"+str(time.time()-start_time)+" seconds"
     return
---
+++ postprocessing_new.py
@@ -0,0 +1,82 @@
+try:
+    import Image
+except ImportError:
+    from PIL import Image
+import time
+import argparse
+import os
+import numpy as np
+import shutil
+
+def create_folder_if_not_exist(path):
+    if not os.path.isdir(path):
+        os.makedirs(path)
+    return
+
+def crop_size_for_video_and_images(input_dir, output_image_dir):
+    create_folder_if_not_exist(output_image_dir)
+    folders = [f for f in sorted(os.listdir(input_dir)) if os.path.isdir(os.path.join(input_dir, f))]
+    print 'folders:', folders   # This should be a list of stroke classes
+
+    # folders should contain bottom_player_winning and top_player_winning.
+    for folder in folders:
+        input_folder_path = os.path.join(input_dir, folder)
+        image_folders = [f for f in sorted(os.listdir(input_folder_path)) if os.path.isdir(os.path.join(input_folder_path, f))]
+        output_image_path = os.path.join(output_image_dir, folder)
+        create_folder_if_not_exist(output_image_path)
+        # if more than 10 image frames, crop them and copy over into one folder
+        for img_folder in image_folders:
+            full_input_folder_name = os.path.join(input_folder_path, img_folder)
+            if len(os.listdir(full_input_folder_name)) <= 10:
+                continue
+            full_img_folder_name = os.path.join(output_image_path, img_folder)
+            create_folder_if_not_exist(full_img_folder_name)
+            for img_file in os.listdir(full_input_folder_name):
+                ii = Image.open(os.path.join(full_input_folder_name, img_file))
+                box = (300, 0, 900, 600)
+                region = ii.crop(box)
+                region.save(os.path.join(full_img_folder_name, img_file))
+                print 'save cropped image to', os.path.join(full_img_folder_name, img_file)
+
+    return
+
+###### CODE BELOW SELECTS LAST SAMPLE_RATE FRAMES (last second) OF EACH POINT AND PUT INTO THE *_selected FOLDER #####
+def select_images_last_10(output_image_dir):
+    assert(os.path.isdir(output_image_dir))
+    folders = [f for f in sorted(os.listdir(output_image_dir)) if os.path.isdir(os.path.join(output_image_dir, f))]
+    # folders should contain bottom_player_winning and top_player_winning.
+    for folder in folders:
+        output_selected_image_path = os.path.join(output_image_dir, folder+'_selected')
+        create_folder_if_not_exist(output_selected_image_path)
+        output_image_path = os.path.join(output_image_dir, folder)
+        assert(os.path.isdir(output_image_path))
+
+        image_folders = [f for f in sorted(os.listdir(output_image_path)) if os.path.isdir(os.path.join(output_image_path, f))]
+        for img_folder in image_folders:
+            full_output_image_path = os.path.join(output_image_path, img_folder)
+
+            files_to_copy = sorted(os.listdir(full_output_image_path))
+            if len(files_to_copy) < 10:
+                continue
+            for f in files_to_copy[-10:]:
+                src = os.path.join(full_output_image_path, f)
+                dest = os.path.join(output_selected_image_path, img_folder+'_'+f)
+                print "DEBUG: copy ", src, " to ", dest
+                shutil.copy(src, dest)
+
+if __name__=='__main__':
+    start_time = time.time()
+    # Parse out the arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_dir", type=str, help='Path to the directory containing a set of stroke classes like top_player_winning, bottom_player_winning etc.')
+    parser.add_argument("image_dir", type=str, help='Path to the output images from the game.')
+    # parser.add_argument("audio_dir", type=str, help='Path to the output audio directory.')
+    args = parser.parse_args()
+    args.input_dir = os.path.abspath(args.input_dir)
+    args.image_dir = os.path.abspath(args.image_dir)
+    # args.audio_dir = os.path.abspath(args.audio_dir)
+    args.audio_dir=None
+    crop_size_for_video_and_images(args.input_dir, args.image_dir)
+    # select_images(args.image_dir, args.audio_dir)
+    select_images_last_10(args.image_dir)
+    print "TOTAL EXECUTION TIME:"+str(time.time()-start_time)+" seconds"
