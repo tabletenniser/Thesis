bb794a1826c4e78e9de55c075070d3e3c35bf804
Zexuan Wang <tabletenniser@gmail.com>
2016-04-20 01:14:11 -0400

    Add new data

--- sqlearn_data_preparation.py
+++ sqlearn_data_preparation.py
@@ -44,9 +44,7 @@ import skimage
 import skimage.io
 import time
 
-# container_path = 'output_labeled_img_dir_5videos_for_pt/'
-# container_path = './output_labeled_img_dir_6videos_for_pt/'
-container_path = './output_labeled_img_dir_8videos_for_pt'
+container_path = './output_labeled_img_dir_8videos_for_pt_trainingset'
 
 pt_folders = [f for f in sorted(os.listdir(container_path)) if os.path.isdir(os.path.join(container_path, f))]
 
@@ -120,7 +118,7 @@ def write_to_data_file(pt_num):
     #                 caffe.TEST)
 
     net = caffe.Net(caffe_root + 'models/finetune_stroke_cls/deploy.prototxt',
-                    caffe_root + 'fine_tune_stroke_cls_500iter.caffemodel',
+                    thesis_root + 'fine_tune_stroke_cls_500iter.caffemodel',
                     caffe.TEST)
 
     # input preprocessing: 'data' is the name of the input blob == net.inputs[0]
@@ -174,7 +172,7 @@ def write_to_data_file(pt_num):
         normalized_result.append((r-np.mean(r))/np.std(r))
     result = normalized_result
 
-    output_file = './seq_data_fc6_normalized_8videos/point_%05d.dat'%(pt_num+1)
+    output_file = './seq_data_fc6_normalized_8videos_trainingset/point_%05d.dat'%(pt_num+1)
     with open(output_file, 'w+') as f:
         for i in xrange(len(result[0])):
             result_lst = [str(elem) for elem in result[0][i]]
